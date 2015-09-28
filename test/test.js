6a6420af3e75d2fdbb60776247ef191d81b4dd3b
Dimitris Sarlis <dsarlis@cslab.ece.ntua.gr>
2015-09-27 18:58:36 -0700

    identation fix #4

--- test/test.js
+++ test/test.js
@@ -11,30 +11,30 @@ describe('Array', function() {
 });
 
 describe('Database', function() {
-	var db;
-	before(function() {
-		db = new sqlite3.Database('ssnoc.db');
-	});
+  var db;
+  before(function() {
+    db = new sqlite3.Database('ssnoc.db');
+  });
 
-	describe('#checkDB', function() {
-		it('should correctly insert and retrieve a new user into database', function() {
-			// create a dump user
-			var username = 'test';
-			var password = 12345;
+  describe('#checkDB', function() {
+    it('should correctly insert and retrieve a new user into database', function() {
+      // create a dump user
+      var username = 'test';
+      var password = 12345;
 
-			db.serialize(function () {
-				// insert the user into the database
-  				var stmt = db.prepare('INSERT INTO users VALUES(?, ?)');
-  				stmt.run(username, password);
-  				stmt.finalize();
+      db.serialize(function () {
+        // insert the user into the database
+        var stmt = db.prepare('INSERT INTO users VALUES(?, ?)');
+        stmt.run(username, password);
+        stmt.finalize();
 
-  				//check that the user can be retrieved from the database
-  				db.get("SELECT username, password FROM users WHERE username='" + username 
-  					+ "' AND password=" + password, function(err, row) {
-  					assert.equal(username, row.username);
-  					assert.equal(password, row.password); 
-  				});
-  			});
-		});
-	});
+        //check that the user can be retrieved from the database
+        db.get("SELECT username, password FROM users WHERE username='" + username 
+          + "' AND password=" + password, function(err, row) {
+          assert.equal(username, row.username);
+          assert.equal(password, row.password); 
+        });
+      });
+    });
+  });
 });
