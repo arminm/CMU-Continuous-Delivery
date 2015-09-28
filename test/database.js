21a04b65c0a08c94de90f1fda2f685955fed5c1d
Dimitris Sarlis <dsarlis@cslab.ece.ntua.gr>
2015-09-28 12:11:36 -0700

    renamed testing file for db

--- test/database.js
+++ test/database.js
@@ -1,15 +1,6 @@
 var assert = require("assert");
 var sqlite3 = require("sqlite3").verbose();
 
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});
-
 describe('Database', function() {
   var db;
   before(function() {
@@ -29,8 +20,8 @@ describe('Database', function() {
         stmt.finalize();
 
         //check that the user can be retrieved from the database
-        db.get("SELECT username, password FROM users WHERE username='" + username 
-          + "' AND password=" + password, function(err, row) {
+        db.all("SELECT username, password FROM users WHERE username='" + username 
+          + "' AND password='" + password + "'", function(err, row) {
           assert.equal(username, row.username);
           assert.equal(password, row.password); 
         });
