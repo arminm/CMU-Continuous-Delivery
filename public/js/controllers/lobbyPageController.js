5fe0144d81545bcc37af7c45c4d705393e05f9b8
MandyChen0114 <jiayaochen93@gmail.com>
2015-10-08 18:48:31 -0700

    Add UI for directory and lobby page

--- public/js/controllers/lobbyPageController.js
+++ public/js/controllers/lobbyPageController.js
@@ -1,5 +1,12 @@
 angular.module('myApp')
 .controller('lobbyPageController', function($scope) {
-
+	$scope.username = 'Mandy';
+	$scope.oneAtATime = true;
+    $scope.onlineitems = ['onlineUser 1', 'onlineUser 2', 'onlineUser 3'];
+    $scope.offlineitems = ['offlineUser 1', 'offlineUser 2', 'offlineUser 3'];
+    $scope.firstwelcome = true;
+    $scope.closewelcome = function () {
+      $scope.firstwelcome=false;
+    };
  });
 
