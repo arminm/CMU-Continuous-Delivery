b7218e60e8dfe17c0fa1a012d06352ecade08a75
MandyChen0114 <jiayaochen93@gmail.com>
2015-10-08 23:58:41 -0700

    [See Directory: Front End] Add tabs in the lobby page

--- public/js/controllers/lobbyPageController.js
+++ public/js/controllers/lobbyPageController.js
@@ -5,8 +5,26 @@ angular.module('myApp')
 	$scope.onlineitems = ['onlineUser 1', 'onlineUser 2', 'onlineUser 3'];
 	$scope.offlineitems = ['offlineUser 1', 'offlineUser 2', 'offlineUser 3'];
 	$scope.firstwelcome = User.checkFirstTimeUser();
+	$scope.seedirectory = false;
+	$scope.seelobby = false;
+	$scope.seesettings = false;
 	$scope.closewelcome = function () {
 		$scope.firstwelcome = false;
+	};
+	$scope.tabDirectory = function () {
+		$scope.seedirectory = true;
+		$scope.seelobby = false;
+		$scope.seesettings = false;
+	};
+	$scope.tabLobby = function () {
+		$scope.seedirectory = false;
+		$scope.seelobby = true;
+		$scope.seesettings = false;
+	};
+	$scope.tabSettings = function () {
+		$scope.seedirectory = false;
+		$scope.seelobby = false;
+		$scope.seesettings = true;
 	};
 	$scope.logout = function () {
 		JoinCommunity.logout($scope.username)
