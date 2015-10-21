3d4e1ff666155523234ee15efc0d631dafa2a751
Armin Mahmoudi <armin@mobovivo.com>
2015-10-21 12:09:18 -0700

    Updated .gitignore and indentation in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,31 @@
 module.exports = function(grunt) {
-	// Project configuration
-	grunt.initConfig ({
-		pkg: grunt.file.readJSON('package.json'),
-		mochaTest: {
-			test: {
-				options: {
-					ui: 'tdd',
-					reporter: 'spec',
-					quiet: false,
-					clearRequireCache: false
-				},
-				src: ['test/**/*.js']
-			},
-			circle: {
-				options: {
-					ui: 'tdd',
-					reporter: 'mocha-junit-reporter',
-					quiet: false,
-					reporterOptions: {
-						mochaFile: process.env.CIRCLE_TEST_REPORTS + '/mocha/results.xml'
-					}
-				},
-				src: ['test/**/*.js']
-			}
-		},
-		mocha_istanbul: {
-			coverage: {
+    // Project configuration
+    grunt.initConfig ({
+        pkg: grunt.file.readJSON('package.json'),
+        mochaTest: {
+            test: {
+                options: {
+                    ui: 'tdd',
+                    reporter: 'spec',
+                    quiet: false,
+                    clearRequireCache: false
+                },
+                src: ['test/**/*.js']
+            },
+            circle: {
+                options: {
+                    ui: 'tdd',
+                    reporter: 'mocha-junit-reporter',
+                    quiet: false,
+                    reporterOptions: {
+                        mochaFile: process.env.CIRCLE_TEST_REPORTS + '/mocha/results.xml'
+                    }
+                },
+                src: ['test/**/*.js']
+            }
+        },
+        mocha_istanbul: {
+            coverage: {
                 src: 'test/**/*.js', // a folder works nicely
                 options: {
                     mochaOptions: ['--ui', 'tdd'], // any extra options for mocha
@@ -35,16 +35,16 @@ module.exports = function(grunt) {
         }
     });
 
-	// Load tasks
-	grunt.loadNpmTasks('grunt-mocha-test');
-	grunt.loadNpmTasks('grunt-mocha-istanbul');
+    // Load tasks
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-	// Register tasks (Both `$ grunt` and `$ grunt test` would run mochaTest)
-	grunt.registerTask('default', ['mochaTest:test']);
-	grunt.registerTask('test', ['mochaTest:test']);
+    // Register tasks (Both `$ grunt` and `$ grunt test` would run mochaTest)
+    grunt.registerTask('default', ['mochaTest:test']);
+    grunt.registerTask('test', ['mochaTest:test']);
 
-	// Circle
-	grunt.registerTask('circle', ['mochaTest:circle', 'mocha_istanbul']);
+    // Circle
+    grunt.registerTask('circle', ['mochaTest:circle', 'mocha_istanbul']);
 
     //Coverage
     grunt.registerTask('coverage', ['mocha_istanbul']);
