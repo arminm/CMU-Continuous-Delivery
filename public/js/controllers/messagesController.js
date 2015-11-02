4fd0dbabd8278be85a4fda9f261ee255741c6740
Dimitris Sarlis <sarlis.dim@gmail.com>
2015-11-01 21:44:23 -0800

    Filtering for stop words

--- public/js/controllers/messagesController.js
+++ public/js/controllers/messagesController.js
@@ -7,6 +7,7 @@ angular.module('myApp')
 	$scope.searchText = '';
 	$scope.descending = false;
 	$scope.searchMode = false;
+	$scope.stopWords = ['a', 'able', 'about', 'across', 'after', 'all', 'almost', 'also', 'am', 'among', 'an', 'and', 'any', 'are', 'as', 'at', 'be', 'because', 'been', 'but', 'by', 'can', 'cannot', 'could', 'dear', 'did', 'do', 'does', 'either', 'else', 'ever', 'every', 'for', 'from', 'get', 'got', 'had', 'has', 'have', 'he', 'her', 'hers', 'him', 'his', 'how', 'however', 'i', 'if', 'in', 'into', 'is', 'it', 'its', 'just', 'least', 'let', 'like', 'likely', 'may', 'me', 'might', 'most', 'must', 'my', 'neither', 'no', 'nor', 'not', 'of', 'off', 'often', 'on', 'only', 'or', 'other', 'our', 'own', 'rather', 'said', 'say', 'says', 'she', 'should', 'since', 'so', 'some', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'these', 'they', 'this', 'tis', 'to', 'too', 'twas', 'us', 'wants', 'was', 'we', 'were', 'what', 'when', 'where', 'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'would', 'yet', 'you', 'your'];
 	switch ($state.$current.url.sourcePath) {
 		case '/lobby/announcements':
 			$scope.title = "Announcements";
@@ -64,10 +65,33 @@ angular.module('myApp')
 		$scope.descending = false;
 	};
 
+	$scope.filterMessages = function() {
+		$scope.getAllMessages();
+		filteredMessages = [];
+		for (var i =  0; i < $scope.messages.length; i++) {
+			contents = $scope.messages[i].content.toLowerCase().split(' ');
+			for (var j = 0; j < contents.length; j++) {
+				if ($scope.filteredParam.indexOf(contents[j]) > -1) {
+					filteredMessages.push($scope.messages[i]);
+					console.log($scope.messages[i]);
+					break;
+				}
+			}
+		}
+		return filteredMessages;
+	};
+
 	$scope.search = function(param) {
 		$scope.searchMode = true;
 		if (param !== '') {
-			$scope.searchText = param;
+			params = param.toLowerCase().split(' ');
+			$scope.filteredParam = [];
+			for (var i = 0; i < params.length; i++) {
+				if ($scope.stopWords.indexOf(params[i]) === -1) {
+					$scope.filteredParam.push(params[i]);
+				}
+			}
+			$scope.messages = $scope.filterMessages();
 			if ($scope.limitResults === 1000000) {
 				$scope.descending = true;
 				$scope.limitResults = 10;
