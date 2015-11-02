172f719916c0f9ef1578631b77ec6bec52b84cdb
Pragya Tripathi <pragyat@andrew.cmu.edu>
2015-11-01 23:11:17 -0800

    [Measure Performance: Front End] Add back to back requests; Improve timer

--- public/js/controllers/maintenanceController.js
+++ public/js/controllers/maintenanceController.js
@@ -3,6 +3,7 @@ angular.module('myApp')
 	$scope.duration = null;
 	$scope.testMode = false;
 	$scope.resultMode = false;
+	$scope.username = User.getUsername();
 	$scope.start = function() {
 		MaintenanceFactory.post($scope.username)
 		.success(function(data) {
@@ -17,10 +18,12 @@ angular.module('myApp')
 		$scope.testMode = true;
 		$scope.resultMode = false;
 		$scope.startTimer();
+		// $scope.makeRequests();
 	};
 	$scope.abort = function() {
 		$scope.testMode = false;
 		$scope.resultMode = true;
+		$scope.stopTimer();
 	};
 	$scope.stop = function() {
 		MaintenanceFactory.delete($scope.username)
@@ -33,24 +36,49 @@ angular.module('myApp')
 
 	};
 
-    var stop;
+	$scope.makeRequests = function() {
+		var messageData = {
+	        target: null,
+	        // The below message is exactly 20 characters long
+	        content: '20 character message',
+	        messageType: 'WALL',
+	        postedAt: Date.now()
+	    };
+    	do {
+    		MaintenanceFactory.postWallMessage($scope.username, messageData)
+    		.success(function(data) {
+			})
+			.error(function(data) {
+				$scope.stop();
+			});
+			MaintenanceFactory.getAllWallMessages($scope.username)
+    		.success(function(data) {
+			})
+			.error(function(data) {
+				$scope.stop();
+			});
+		}
+		while ($scope.testMode);
+    };
+
+    var stopper;
     $scope.startTimer = function() {
       // Don't start a new timer if we are already running one
-      if ( angular.isDefined(stop) ) return;
+      if ( angular.isDefined(stopper) ) return;
       $scope.elapsedTime = $scope.duration;
-      stop = $interval(function() {
+      stopper = $interval(function() {
         if ($scope.elapsedTime > 0) {
           $scope.elapsedTime--;
         } else {
-          $scope.stopTimer();
+          $scope.stop();
         }
       }, 1000);
     };
 
     $scope.stopTimer = function() {
-      if (angular.isDefined(stop)) {
-        $interval.cancel(stop);
-        stop = undefined;
+      if (angular.isDefined(stopper)) {
+        $interval.cancel(stopper);
+        stopper = undefined;
       }
     };
 });
--- public/js/services/maintenanceService.js
+++ public/js/services/maintenanceService.js
@@ -4,12 +4,18 @@ angular.module('MaintenanceService', [])
 
 })
 .factory('MaintenanceFactory', function($http) {
-        return {
-            post : function(username) {
-                return $http.post('/maintenance?access_key=' + username);
-            },
-            delete : function(username) {
-                return $http.delete('/maintenance?access_key=' + username);
-            }
+    return {
+        post : function(username) {
+            return $http.post('/maintenance?access_key=' + username);
+        },
+        delete : function(username) {
+            return $http.delete('/maintenance?access_key=' + username);
+        },
+        postWallMessage : function(username, data) {
+            return $http.post('/messages/' + username + '?access_key=' + username, data);
+        },
+        getAllWallMessages : function(username) {
+            return $http.get('/messages?messageType=WALL&access_key=' + username);
         }
-    });
+    }
+});
