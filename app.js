343e862c32c2aa3081046c9049dd338c39e7ab91
Pragya Tripathi <pragyat@andrew.cmu.edu>
2015-11-28 21:08:16 -0800

    [Administer Profile: Back-end] Delete admin endpoints

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var messages = require('./routes/messages');
 var status = require('./routes/status');
-var admin = require('./routes/admin');
-var routers = [ users, routes, messages, status, admin ];
+var routers = [ users, routes, messages, status ];
 
 var maintenance = require('./routes/maintenance');
 var app = express();
@@ -44,7 +43,6 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/messages', messages);
 app.use('/status', status);
-app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
