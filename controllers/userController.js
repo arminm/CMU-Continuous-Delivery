1fe43787a579c51f99e416a2c647cfc37fadb5c9
Pragya Tripathi <pragyat@andrew.cmu.edu>
2015-11-28 21:08:48 -0800

    [Administer Profile: Back-end] Delete status crumbs update while updating user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,20 @@ var Utils = require('../utilities.js');
 
 module.exports = {
 	getAllUsers: function(req, res) {
-		User.getAllUsers(true, function(users, error) {
+		User.get(req.params.access_key, function(user, password, error) {
 			if (error) {
 				res.sendStatus(500);
+			} else if (user) {
+				var activeOnly = (user.profile != 'ADMINISTRATOR');
+				User.getAllUsers(activeOnly, function(users, error) {
+					if (error) {
+						res.sendStatus(500);
+					} else {
+						res.status(200).send(users);
+					}
+				});
 			} else {
-				res.status(200).send(users);
+				res.sendStatus(404);
 			}
 		});
 	},
@@ -18,6 +27,29 @@ module.exports = {
 				res.sendStatus(500);
 			} else if (user) {
 				res.status(200).send(user);
+			} else {
+				res.sendStatus(404);
+			}
+		});
+	},
+
+	updateUser: function(req, res) {
+		User.get(req.params.access_key, function(user, password, error) {
+			if (error) {
+				res.sendStatus(500);
+			} else if (user && user.profile == 'ADMINISTRATOR') {
+				var userInfo = {
+					isActive: req.body.isActive,
+					givenUsername: req.body.givenUsername,
+					password: req.body.password,
+					profile: req.body.profile,
+					username: req.params.username
+				};
+				User.updateUser(userInfo, function(success, error) {
+					if (error) {
+						res.sendStatus(400);
+					}
+				});
 			} else {
 				res.sendStatus(404);
 			}
