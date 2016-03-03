5a6346980a2c9d4efdf0021ef453eaf700d0c6d1
Armin Mahmoudi <arminm89@gmail.com>
2016-03-03 12:38:09 -0800

    Fixed bower installation

--- server.sh
+++ server.sh
@@ -1,6 +1,7 @@
 npm install -g grunt-cli
 npm install
+npm install bower
 ./node_modules/bower/bin/bower install
 npm install sqlite3
-node_modules/db-migrate/bin/db-migrate up
+./node_modules/db-migrate/bin/db-migrate up
 npm start
