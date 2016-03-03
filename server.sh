b90e86f886addd77e36da9ee1b64c7de7eb00a5b
Armin Mahmoudi <arminm89@gmail.com>
2016-03-03 12:31:09 -0800

    Updated server.sh

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 npm install -g grunt-cli
-npm install -g bower
-npm setup
+npm install
+./node_modules/bower/bin/bower install
 npm install sqlite3
 node_modules/db-migrate/bin/db-migrate up
 npm start
